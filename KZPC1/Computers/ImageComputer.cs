55fa1a071bab01410c75ce81eb41e2c682b2c7a3
Michal <michal.hvezda@softgate.cz>
2021-10-16 16:49:12 +0200

    ImageComputer - changes

--- KZPC1/Computers/ImageComputer.cs
+++ KZPC1/Computers/ImageComputer.cs
@@ -28,84 +28,33 @@ namespace KZPC1.Computers
             MaskUseNumber = 1;
         }
 
-        //public virtual RenderTarget2D AvgPhoto(Texture2D frontFoto, int width, int height, bool fotoDispose = false)
-        //{
-        //    RenderTarget2D renderTarget = new RenderTarget2D(
-        //    GraphicsDevice,
-        //    width / 2, height / 2, true,
-        //    SurfaceFormat.Color, DepthFormat.Depth16);
-
-        //    //nastaveni shaderu
-        //    Effect.Parameters["MainTexture"].SetValue(frontFoto);
-        //    Effect.Parameters["width"].SetValue((float)width);
-        //    Effect.Parameters["height"].SetValue((float)height);
-        //    Effect.CurrentTechnique.Passes[2].Apply();
-
-
-        //    _graphicsDevice.SetRenderTarget(renderTarget);
-        //    //ukazani na obrazek odkud pokud se bude spracovavat, podstatě rohy obrazku
-        //    _graphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 2);
-        //    _graphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 3, 5);
-
-        //    _graphicsDevice.SetRenderTargets(null);
-        //    _graphicsDevice.Clear(new Color(1, 1, 1, 1));
-
-        //    /*
-        //    Console.WriteLine("Level is " + renderTarget.LevelCount);
-        //    Color[] col = new Color[1];
-        //    renderTarget.GetData(renderTarget.LevelCount-1, null,col,0,1);
-        //    foreach (var c in col)
-        //    {
-        //        Console.WriteLine(c.ToVector4().ToString() + " !!!!!!!!!!!");
-        //    }
-        //    */
-        //    /*if (renderTarget.Height < 100)
-        //    {
-
-        //        WriteColToConsoleFromPicture(renderTarget);
-        //    }*/
-        //    //Console.WriteLine("Height is " + (height / 2) + " and width is " + (width / 2));
-        //    if (fotoDispose)
-        //    {
-        //        frontFoto.Dispose();
-        //    }
-        //    if (height > 2 && width > 2)
-        //    {
-        //        return AvgPhoto(renderTarget, width / 2, height / 2, true);
-        //    }
-        //    else
-        //    {
-        //        return renderTarget;
-        //    }
-        //}
-
-        //public virtual Vector3 AvgPhotoVec(Texture2D frontFoto, int width, int height)
-        //{
-        //    Color colF = new Color();
-        //    using (var jjo = AvgPhoto(frontFoto, width, height))
-        //    {
-        //        Color[] col = new Color[jjo.Height * jjo.Width];
-        //        jjo.GetData<Color>(col);
-        //        foreach (var c in col)
-        //        {
-        //            //Console.WriteLine(c.ToVector4().ToString());
-        //            colF = c;
-        //        }
-        //    }
-        //    //Console.WriteLine("out " + colF.ToVector4().ToString());
-        //    //frontFoto.Dispose();
-        //    return colF.ToVector3();
-        //}
-
         public virtual void BufPhotoMain()
         {
             if (_mask != null)
                 _mask.Dispose();
 
-            _mask = BufPhoto();
+            _mask = BufPhotoBitMap();
         }
 
-        public virtual RenderTarget2D BufPhoto()
+        public virtual RenderTarget2D UnionPhoto()
+        {
+            RenderTarget2D renderTarget;
+
+            //nastavení objektu ktery bude počitan na shaderu
+            renderTarget = new RenderTarget2D(
+            GraphicsDevice,
+            _defaulSize, _defaulSize, false,
+            SurfaceFormat.Color, DepthFormat.Depth16);
+
+            //nastaveni shaderu
+            SetEffects(3, MainTexture: Foto, BackGround: Back, MeansTexture: _mask);
+
+            SetRenderTargetAndDraw(renderTarget);
+            return renderTarget;
+        }
+
+        #region bip map reduction
+        public virtual RenderTarget2D BufPhotoBitMap()
         {
             RenderTarget2D renderTarget1, renderTarget2, renderTarget3;
 
@@ -148,6 +97,81 @@ namespace KZPC1.Computers
                 renderTarget3.Dispose();
                 Console.WriteLine("v1 " + v1.ToString() + " v2 " + v2.ToString() + " v3 " + v3.ToString());
                 SetVectors(v1, v2, v3);
+                return BufPhotoBitMap();
+            }
+            else
+            {
+                SetVectors(v1, v2, v3);
+                if (MaskUseNumber == 1)
+                {
+                    renderTarget2.Dispose();
+                    renderTarget3.Dispose();
+                    return renderTarget1;
+                }
+                if (MaskUseNumber == 2)
+                {
+                    renderTarget1.Dispose();
+                    renderTarget3.Dispose();
+                    return renderTarget2;
+                }
+                if (MaskUseNumber == 3)
+                {
+                    renderTarget1.Dispose();
+                    renderTarget2.Dispose();
+                    return renderTarget3;
+                }
+            }
+            renderTarget1.Dispose();
+            renderTarget2.Dispose();
+            renderTarget3.Dispose();
+            return new RenderTarget2D(GraphicsDevice, _defaulSize, _defaulSize);
+        }
+        #endregion
+
+        #region shader reduction
+        public virtual RenderTarget2D BufPhoto()
+        {
+            RenderTarget2D renderTarget1, renderTarget2, renderTarget3;
+
+            //nastavení objektu ktery bude počitan na shaderu
+            renderTarget1 = new RenderTarget2D(
+            GraphicsDevice,
+            _defaulSize, _defaulSize, true,
+            SurfaceFormat.Color, DepthFormat.Depth16);
+
+            renderTarget2 = new RenderTarget2D(
+            GraphicsDevice,
+            _defaulSize, _defaulSize, true,
+            SurfaceFormat.Color, DepthFormat.Depth16);
+
+            renderTarget3 = new RenderTarget2D(
+            GraphicsDevice,
+            _defaulSize, _defaulSize, true,
+            SurfaceFormat.Color, DepthFormat.Depth16);
+            GraphicsDevice.BlendState = BlendState.Opaque;
+
+            //nastaveni shaderu
+            SetEffects(0, MainTexture: Foto, Cent1: Vector1, Cent2: Vector2, Cent3: Vector3);
+
+            SetRenderTargetAndDraw(renderTarget1, renderTarget2, renderTarget3);
+
+            //return new List<RenderTarget2D> { renderTarget1, renderTarget2, renderTarget3 };
+            //var v1 = AvgPhotoVec(renderTarget1, renderTarget1.Width, renderTarget1.Height);
+            //var v2 = AvgPhotoVec(renderTarget2, renderTarget2.Width, renderTarget2.Height);
+            //var v3 = AvgPhotoVec(renderTarget3, renderTarget3.Width, renderTarget3.Height);
+
+            var v1 = AvgPhotoVec(renderTarget1);
+            var v2 = AvgPhotoVec(renderTarget2);
+            var v3 = AvgPhotoVec(renderTarget3);
+
+            //jestli se jiz centroidy stabilizovali
+            if (!Vector1.Equals(v1) && !Vector2.Equals(v2) && !Vector3.Equals(v3))
+            {
+                renderTarget1.Dispose();
+                renderTarget2.Dispose();
+                renderTarget3.Dispose();
+                Console.WriteLine("v1 " + v1.ToString() + " v2 " + v2.ToString() + " v3 " + v3.ToString());
+                SetVectors(v1, v2, v3);
                 return BufPhoto();
             }
             else
@@ -178,14 +202,68 @@ namespace KZPC1.Computers
             return new RenderTarget2D(GraphicsDevice, _defaulSize, _defaulSize);
         }
 
-        //public virtual void Calculate()
-        //{
-        //    SetVectorsForDraw();
-        //    //_listTargets.Clear();
-        //    _listTargets = BufPhoto(_foto, _vector1, _vector2, _vector3);
-        //    //SaveTextures();
-        //}
+        public virtual RenderTarget2D AvgPhoto(Texture2D frontFoto, bool fotoDispose = false)
+        {
+            RenderTarget2D renderTarget = new RenderTarget2D(
+            GraphicsDevice,
+            frontFoto.Width / 2, frontFoto.Height / 2, true,
+            SurfaceFormat.Color, DepthFormat.Depth16);
 
+            //nastaveni shaderu
+            SetEffects(2, MainTexture: frontFoto, Width: frontFoto.Width, Height: frontFoto.Height);
+
+
+            SetRenderTargetAndDraw(renderTarget);
+
+            /*
+            Console.WriteLine("Level is " + renderTarget.LevelCount);
+            Color[] col = new Color[1];
+            renderTarget.GetData(renderTarget.LevelCount-1, null,col,0,1);
+            foreach (var c in col)
+            {
+                Console.WriteLine(c.ToVector4().ToString() + " !!!!!!!!!!!");
+            }
+            */
+            /*if (renderTarget.Height < 100)
+            {
+
+                WriteColToConsoleFromPicture(renderTarget);
+            }*/
+            //Console.WriteLine("Height is " + (height / 2) + " and width is " + (width / 2));
+            if (fotoDispose)
+            {
+                frontFoto.Dispose();
+            }
+            if (frontFoto.Width > 2 && frontFoto.Height > 2)
+            {
+                return AvgPhoto(renderTarget, true);
+            }
+            else
+            {
+                return renderTarget;
+            }
+        }
+
+        public virtual Vector3 AvgPhotoVec(Texture2D frontFoto)
+        {
+            Color colF = new Color();
+            using (var jjo = AvgPhoto(frontFoto))
+            {
+                Color[] col = new Color[jjo.Height * jjo.Width];
+                jjo.GetData<Color>(col);
+                foreach (var c in col)
+                {
+                    //Console.WriteLine(c.ToVector4().ToString());
+                    colF = c;
+                }
+            }
+            //Console.WriteLine("out " + colF.ToVector4().ToString());
+            //frontFoto.Dispose();
+            return colF.ToVector3();
+        }
+        #endregion
+
+        #region draw
         public virtual void DrawMask(SpriteBatch spriteBatch, Rectangle windowSize)
         {
             spriteBatch.Begin();
@@ -246,23 +324,6 @@ namespace KZPC1.Computers
         //    return renderTarget;
         //}
 
-        public virtual RenderTarget2D UnionPhoto()
-        {
-            RenderTarget2D renderTarget;
-
-            //nastavení objektu ktery bude počitan na shaderu
-            renderTarget = new RenderTarget2D(
-            GraphicsDevice,
-            _defaulSize, _defaulSize, false,
-            SurfaceFormat.Color, DepthFormat.Depth16);
-
-            //nastaveni shaderu
-            SetEffects(3, MainTexture: Foto, BackGround: Back, MeansTexture: _mask);
-
-            SetRenderTargetAndDraw(renderTarget);
-            return renderTarget;
-        }
-
         public virtual void DrawFinal(SpriteBatch spriteBatch, Rectangle windowSize)
         {
             SetVectorsForDraw();
@@ -277,116 +338,9 @@ namespace KZPC1.Computers
         {
             SetVectorsForDraw();
             BufPhotoMain();
-            SetVectorsForDraw();
-            using (var image = UnionPhoto())
-            {
-                spriteBatch.Begin();
-                spriteBatch.Draw(image, windowSize, Color.White);
-                spriteBatch.End();
-            }
+            DrawFinal(spriteBatch, windowSize);
         }
-
-        //public virtual RenderTarget2D BufPhoto(Texture2D frontFoto, Vector3 vector1, Vector3 vector2, Vector3 vector3, int targetNumber)
-        //{
-        //    RenderTarget2D renderTarget1, renderTarget2, renderTarget3;
-
-        //    //nastavení objektu ktery bude počitan na shaderu
-        //    renderTarget1 = new RenderTarget2D(
-        //    _graphicsDevice,
-        //    4096, 4096, false,
-        //    SurfaceFormat.Color, DepthFormat.Depth16);
-
-        //    renderTarget2 = new RenderTarget2D(
-        //    _graphicsDevice,
-        //    4096, 4096, false,
-        //    SurfaceFormat.Color, DepthFormat.Depth16);
-
-        //    renderTarget3 = new RenderTarget2D(
-        //    _graphicsDevice,
-        //    4096, 4096, false,
-        //    SurfaceFormat.Color, DepthFormat.Depth16);
-        //    _graphicsDevice.BlendState = BlendState.Opaque;
-        //    //nastaveni shaderu
-        //    _effect.Parameters["MainTexture"].SetValue(frontFoto);
-        //    _effect.Parameters["cent1"].SetValue(vector1);
-        //    _effect.Parameters["cent2"].SetValue(vector2);
-        //    _effect.Parameters["cent3"].SetValue(vector3);
-        //    _effect.CurrentTechnique.Passes[0].Apply();
-        //    _graphicsDevice.SetRenderTargets(renderTarget1, renderTarget2, renderTarget3);
-        //    //ukazani na obrazek odkud pokud se bude spracovavat, podstatě rohy obrazku
-        //    _graphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 2);
-        //    _graphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 3, 5);
-
-        //    _graphicsDevice.SetRenderTargets(null);
-        //    _graphicsDevice.Clear(new Color(1, 1, 1, 1));
-        //    var v1 = AvgPhotoVec(renderTarget1, renderTarget1.Width, renderTarget1.Height);
-        //    var v2 = AvgPhotoVec(renderTarget2, renderTarget2.Width, renderTarget2.Height);
-        //    var v3 = AvgPhotoVec(renderTarget3, renderTarget3.Width, renderTarget3.Height);
-
-        //    if (!vector1.Equals(v1) && !vector2.Equals(v2) && !vector3.Equals(v3))
-        //    {
-        //        renderTarget1.Dispose();
-        //        renderTarget2.Dispose();
-        //        renderTarget3.Dispose();
-        //        return BufPhoto(frontFoto, v1, v2, v3, targetNumber);
-        //    }
-        //    else
-        //    {
-        //        //frontFoto.Dispose();
-        //        //SetVectors(v1, v2, v3);
-        //        if (targetNumber == 1)
-        //        {
-        //            renderTarget2.Dispose();
-        //            renderTarget3.Dispose();
-        //            return renderTarget1;
-        //        }
-        //        if (targetNumber == 2)
-        //        {
-        //            renderTarget1.Dispose();
-        //            renderTarget3.Dispose();
-        //            return renderTarget2;
-        //        }
-        //        if (targetNumber == 3)
-        //        {
-        //            renderTarget1.Dispose();
-        //            renderTarget2.Dispose();
-        //            return renderTarget3;
-        //        }
-        //    }
-        //    renderTarget1.Dispose();
-        //    renderTarget2.Dispose();
-        //    renderTarget3.Dispose();
-        //    return new RenderTarget2D(_graphicsDevice, frontFoto.Width, frontFoto.Height);
-        //}
-
-        //public RenderTarget2D UnionPhoto(Texture2D frontFoto, Texture2D backFoto, Texture2D centroid)
-        //{
-        //    RenderTarget2D renderTarget;
-
-        //    //nastavení objektu ktery bude počitan na shaderu
-        //    renderTarget = new RenderTarget2D(
-        //    _graphicsDevice,
-        //    frontFoto.Width, frontFoto.Height, false,
-        //    SurfaceFormat.Color, DepthFormat.Depth16);
-
-        //    //nastaveni shaderu
-        //    _effect.Parameters["MainTexture"].SetValue(frontFoto);
-        //    _effect.Parameters["BackGround"].SetValue(backFoto);
-        //    _effect.Parameters["MeansTexture"].SetValue(centroid);
-        //    _effect.CurrentTechnique.Passes[3].Apply();
-
-        //    _graphicsDevice.SetRenderTarget(renderTarget);
-        //    //ukazani na obrazek odkud pokud se bude spracovavat, podstatě rohy obrazku
-        //    _graphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 2);
-        //    _graphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 3, 5);
-
-        //    _graphicsDevice.SetRenderTargets(null);
-        //    _graphicsDevice.Clear(new Color(1, 1, 1, 1));
-        //    //frontFoto.Dispose();
-        //    return renderTarget;
-        //}
-
-
+        #endregion
 
     }
 }
